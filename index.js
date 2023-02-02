0ada9663723934302112adee9391fbad2df28bcd
Gamruss <123782135+Gamruss@users.noreply.github.com>
2023-02-02 21:54:09 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+var files = document.getElementsByClassName('files');
+for (var i = 0; i < files.length; i++) {
+files[i].style.display = 'none';
+}
+});
+
+function showCategory(category) {
+    var files = document.getElementsByClassName('files');
+    for (var i = 0; i < files.length; i++) {
+        files[i].style.display = 'none';
+    }
+    document.getElementsByClassName(category)[0].style.display = 'block';
+    var categories = document.getElementsByClassName('category');
+    for (var i = 0; i < categories.length; i++) {
+        categories[i].style.backgroundColor = '#282828';
+    }
+    document.getElementsByClassName(category)[1].style.backgroundColor = '#580000';
+}
 
 var categories = document.getElementsByClassName('category');
 for (var i = 0; i < categories.length; i++) {
